2747d5681e67f7fbf2a85281197c8ce42abfc632
Anto426 <amanocchio426@gmail.com>
2022-11-01 13:07:24 +0100

    update

--- function/dark-whitemode.js
+++ function/dark-whitemode.js
@@ -1,8 +1,6 @@
 function myFunction() {
 
     var element = document.body;
-    var element1 = document.a;
     element.classList.toggle("white-mode");
-
-    element1.classList.toggle("lidark");
+    $(this).html('Dark Mode');
 }
